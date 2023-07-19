b4c4c9a2ade6e7b662f3ba8169175c9655823523
Mir2226 <gmihran@internet.ru>
2023-07-19 18:50:52 +0400

    write array out of console

--- script/index.js
+++ script/index.js
@@ -31,17 +31,38 @@
 
 // --------------------------------------------------------
 
+    // const checkTargetIndex = (arr, target) => {
+    //     const result = [];
+    //     for (let i = 0; i < arr.length - 2; i++) {
+    //         for (let j = i + 1; j < arr.length - 1; j++) {
+    //         for (let k = j + 1; k < arr.length; k++) {
+    //             if (arr[i] + arr[j] + arr[k] === target) {
+    //             result.push([i, j, k]);
+    //             }
+    //         }
+    //         }
+    //     }
+    //         return result;
+    //     }
+    // console.log(checkTargetIndex([1, 2, 3, 4, 5], 9));
+
+    //--------------------------------------------------
+
     const checkTargetIndex = (arr, target) => {
         const result = [];
-        for (let i = 0; i < arr.length - 2; i++) {
-            for (let j = i + 1; j < arr.length - 1; j++) {
-            for (let k = j + 1; k < arr.length; k++) {
-                if (arr[i] + arr[j] + arr[k] === target) {
-                result.push([i, j, k]);
-                }
+        for (let i = 0; i < arr.length; i++) {
+            for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === target) {
+                result.push([i, j]);
             }
             }
         }
-            return result;
+        return result;
         }
-    console.log(checkTargetIndex([1, 2, 3, 4, 5], 9));
+        const array = [1, 2, 3, 4, 5];
+        console.log(checkTargetIndex(array, 9));
+
+
+
+
+
