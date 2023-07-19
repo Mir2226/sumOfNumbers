1b0509c285accd2d3512274005d66262bbb832d6
Mir2226 <gmihran@internet.ru>
2023-07-19 18:39:36 +0400

    find sum of numbers with  1 for loop

--- script/index.js
+++ script/index.js
@@ -1,4 +1,4 @@
-  const checkTargetIndex = (arr, target) => {
+const checkTargetIndex = (arr, target) => {
     const indices = {};
     for (let i = 0; i < arr.length; i++) {
         const complement = target - arr[i];
@@ -12,64 +12,4 @@
 console.log(checkTargetIndex([
     1, 2, 3, 4, 5
 ], 10));
-// change number in console and return which numbers of arr sum
-    //---------------------------------------------------------
-//     const checkTargetIndex = (arr, target) => {
-//     const result = [];
-//      for (let i = 0; i < arr.length; i++) {
-//          for (let j = i + 1; j < arr.length; j++) {
-//             if (arr[i] + arr[j] === target) {
-//         result.push([i, j]);
-//       }
-//     }
-//   }
-//   return result;
-// }
-// console.log(checkTargetIndex([1, 2, 3, 4, 5, 5], 10));
-
-//---------------------------------------------------------
-//     const checkTargetIndex = (arr, target) => {
-//     const result = [];
-//     for (let i = 0; i < arr.length - 2; i++) {
-//         for (let j = i + 1; j < arr.length - 1; j++) {
-//         for (let k = j + 1; k < arr.length; k++) {
-//             if (arr[i] + arr[j] + arr[k] === target) {
-//             result.push([i, j, k]);
-//             }
-//         }
-//         }
-//     }
-//         return result;
-//     }
-// console.log(checkTargetIndex([1, 2, 3, 4, 5], 9));
-
-//---------------------------------------------------------
-// const checkTargetIndex = (arr, target) => {
-//     const result = [];
-//     for (let i = 0; i < arr.length; i++) {
-//         for (let j = i + 1; j < arr.length; j++) {
-//         if (arr[i] + arr[j] === target) {
-//             result.push([i, j]);
-//         }
-//         }
-//     }
-//     return result;
-//     }
-//     const array = [1, 2, 3, 4, 5];
-//     console.log(checkTargetIndex(array, 9));
-
-
-    // const checkTargetIndex = (arr, target) => {
-    //     const result = [];
-    //     const map = {};
-    //     for (let i = 0; i < arr.length; i++) {
-    //       const complement = target - arr[i];
-    //       if (map[complement] !== undefined) {
-    //         result.push([map[complement], i]);
-    //       }
-    //       map[arr[i]] = i;
-    //     }
-    //     return result;
-    //   }
-    //   const array = [1, 2, 3, 4, 5];
-    //   console.log(checkTargetIndex(array, 9));
+// change number in console and return which numbers of arr  sum
